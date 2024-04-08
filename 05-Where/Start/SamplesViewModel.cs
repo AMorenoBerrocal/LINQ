2a350bf3e60a2097a41b1ac1c5019c9c9a3afdb4
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-08 08:57:40 +0200

    consulta - where con dos parametros

--- 05-Where/Start/SamplesViewModel.cs
+++ 05-Where/Start/SamplesViewModel.cs
@@ -44,10 +44,11 @@
       List<Product> products = GetProducts();
       List<Product> list = new();
 
-      // Write Query Syntax Here
+            // Write Query Syntax Here
+            list = (from prod in products where prod.Name.StartsWith("L") && prod.StandardCost > 200 select prod).ToList();
 
 
-      return list;
+            return list;
     }
     #endregion
 
@@ -60,10 +61,11 @@
       List<Product> products = GetProducts();
       List<Product> list = new();
 
-      // Write Method Syntax Here
+            // Write Method Syntax Here
+            list = products.Where(prod => prod.Name.StartsWith("S") && prod.StandardCost > 200).ToList();
 
 
-      return list;
+            return list;
     }
     #endregion
 
